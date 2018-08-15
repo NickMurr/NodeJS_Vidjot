a68a763549210bbca87cf064eea3ddbee4ee80cc
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-08-15 12:01:56 +0300

    Fetching Ideas From MongoDB

--- app.js
+++ app.js
@@ -44,6 +44,17 @@ app.get('/about', (req, res) => {
   res.render('ABOUT');
 });
 
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({ date: 'desc' })
+    .then((ideas) => {
+      res.render('ideas/index', {
+        ideas,
+      });
+    });
+});
+
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
