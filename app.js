a4ae36292736041965ec5e346f6ad7fa1cb808d8
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-08-15 11:35:14 +0300

    Server Side Form Validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://localhost/vidjot-dev', {
 
 // Load Idea Model
 require('./models/Idea');
+
 const Idea = mongoose.model('ideas');
 
 // Handlebars middleware
@@ -23,6 +25,10 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 // Index Route
@@ -41,6 +47,28 @@ app.get('/about', (req, res) => {
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+  const errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: 'Please add a title' });
+  }
+  if (!req.body.details) {
+    errors.push({ text: 'Please add a details' });
+  }
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details,
+    });
+  } else {
+    res.send('passed');
+  }
 });
 
 const port = 5000;
