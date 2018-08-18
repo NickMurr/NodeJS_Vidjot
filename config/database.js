482485c0cd1c7ca662fb054b7a977670c591969e
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-08-18 16:45:48 +0300

    Preparing Our App For Deployment

---
+++ config/database.js
@@ -0,0 +1,5 @@
+if (process.env.NODE_ENV === 'production') {
+  module.exports = { mongoURI: 'mongodb://nick:nikitadiana30@ds125342.mlab.com:25342/vidjot-nick-prod' };
+} else {
+  module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' };
+}
